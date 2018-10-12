40e545a8f50f2474a95992e90d4f3e0b03238584
Ayush Goyal <ayush987goyal@gmail.com>
2018-10-12 19:37:48 +0530

    Remove stuff needed for compose and beanstalk

--- temp.sh
+++
@@ -1,8 +0,0 @@
- docker build -t ayush987goyal/multi-client ./client
- docker build -t ayush987goyal/multi-nginx ./nginx
- docker build -t ayush987goyal/multi-server ./server
- docker build -t ayush987goyal/multi-worker ./worker
- docker push ayush987goyal/multi-client
- docker push ayush987goyal/multi-nginx
- docker push ayush987goyal/multi-server
- docker push ayush987goyal/multi-worker
